0b8b7098f39e112edf748a170921a4a29be3bccc
Oliver Muthomi <oliver.mwitia@gmail.com>
2025-06-05 01:51:59 +0300

    Add request logger middleware to backend app

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+app.use("*", logger());
 
 app.get("/test", (c) => {
   return c.json({ message: "test" });
