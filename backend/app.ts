6e4bd1740567dd222ab0d614c6ce86da1b7f3603
Oliver Muthomi <oliver.mwitia@gmail.com>
2025-06-05 04:29:18 +0300

    Add static file serving for frontend application

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,15 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expenseRoute } from "./route/expense";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.use("*", logger());
 
 app.route("/api/expense", expenseRoute);
+
+app.use("*", serveStatic({ root: "../frontend/dist" }));
+app.get("*", serveStatic({ path: "../frontend/dist/index.html" }));
 
 export default app;
