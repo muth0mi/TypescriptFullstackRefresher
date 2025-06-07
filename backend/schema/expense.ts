5d197606cdebe1836980174fc08cf81481293d35
Oliver Muthomi <oliver.mwitia@gmail.com>
2025-06-07 18:44:12 +0300

    Update expense schema validation rules and field order

--- backend/schema/expense.ts
+++ backend/schema/expense.ts
@@ -3,8 +3,8 @@ import { z } from "@hono/zod-openapi";
 export const totalsSchema = z.object({
   expenses: z
     .number()
-    .positive()
     .describe("Total expenses")
+    .positive()
     .openapi({ example: 1100 }),
 });
 
@@ -16,20 +16,21 @@ export const expenseSchema = z.object({
     .openapi({ example: "982cd8c7-16ae-42b7-9d07-49aff7d4c17e" }),
   amount: z
     .string()
-    .min(3, { message: "Amount must be at least 3 characters" })
-    .max(10, { message: "Amount must be at most 10 characters" })
     .describe("The amount of the expense")
+    .min(1, { message: "Amount must be at least 1 character" })
+    .max(10, { message: "Amount must be at most 10 characters" })
+    .regex(/^\d+(\.\d{1,2})?$/, { message: "Amount must be a valid currency" })
     .openapi({ example: "100.50" }),
   category: z
     .string()
+    .describe("The category of the expense")
     .min(3, { message: "Category must be at least 3 characters" })
     .max(50, { message: "Category must be at most 50 characters" })
-    .describe("The category of the expense")
     .openapi({ example: "Food" }),
   description: z
     .string()
+    .describe("The description of the expense")
     .min(3, { message: "Description must be at least 3 character" })
     .max(100, { message: "Description must be at most 100 characters" })
-    .describe("The description of the expense")
     .openapi({ example: "Grocery shopping" }),
 });
