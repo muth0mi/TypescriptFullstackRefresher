5c9006c812f2b4df8118f9ac222b298dcd6caa24
Oliver Muthomi <oliver.mwitia@gmail.com>
2025-06-11 14:43:40 +0300

    Update package names and remove backend alias

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@backend": path.resolve(__dirname, "../backend"),
     },
   },
   server: {
